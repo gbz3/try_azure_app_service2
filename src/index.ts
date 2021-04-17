5ce8e1bd4bab76436c8a286d8859013ee5e5cdb3
gbz3 <1808483+gbz3@users.noreply.github.com>
2021-04-18 08:51:06 +0900

    post データを確認

--- src/index.ts
+++ src/index.ts
@@ -13,11 +13,13 @@ const app = new Koa()
 import mwServ from 'koa-static'
 import { mwRequestId } from './middlewares/request-id'
 import views from 'koa-views'
+import bodyParser from 'koa-bodyparser'
 import { mwRouterWeb } from './middlewares/router-web'
 
 app.use(mwServ(__dirname + '/../static'))
 app.use(mwRequestId)
 app.use(views(getViewRoot(__dirname), { autoRender: true, extension: 'pug' }))
+app.use(bodyParser())
 app.use(mwRouterWeb)
 
 // サーバ起動
--- src/middlewares/router-web.ts
+++ src/middlewares/router-web.ts
@@ -5,6 +5,7 @@ import compose from 'koa-compose'
 // see: https://github.com/DefinitelyTyped/DefinitelyTyped/issues/36161#issuecomment-571295417
 const RouterWeb = new Router<Koa.DefaultState, Koa.Context>()
   .get('/', root)
+  .post('/', request)
   .get('/room/dummy', async ctx => await ctx.render('room'))  // TODO dummy
 
 export const mwRouterWeb = compose([RouterWeb.routes(), RouterWeb.allowedMethods()])
@@ -12,4 +13,9 @@ export const mwRouterWeb = compose([RouterWeb.routes(), RouterWeb.allowedMethods
 async function root(ctx: Koa.Context) {
   ctx.logger.info(`from @koa/router.`)
   await ctx.render('index')
+}
+
+/** 利用申込 */
+async function request(ctx: Koa.Context) {
+  ctx.logger.info(`body=${JSON.stringify(ctx.request.body)}`)
 }
