471ce9752dcf74010f560c7c2f384a9c8f0486c8
gbz3 <1808483+gbz3@users.noreply.github.com>
2021-04-17 20:23:01 +0900

    @koa/router 導入

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,11 @@ const app = new Koa()
 // ミドルウェア設定
 import mwServ from 'koa-static'
 import { mwRequestId } from './middlewares/request-id'
+import { mwRouterWeb } from './middlewares/router-web'
 
 app.use(mwServ(__dirname + '/../static'))
 app.use(mwRequestId)
-app.use(async (ctx) => ctx.body = "koa app. + koa-static")
+app.use(mwRouterWeb)
 
 // サーバ起動
 const port = process.env.PORT || 3000
---
+++ src/middlewares/router-web.ts
@@ -0,0 +1,14 @@
+import Router from '@koa/router'
+import Koa from 'koa'
+import compose from 'koa-compose'
+
+// see: https://github.com/DefinitelyTyped/DefinitelyTyped/issues/36161#issuecomment-571295417
+const RouterWeb = new Router<Koa.DefaultState, Koa.Context>()
+  .get('/', root)
+
+export const mwRouterWeb = compose([RouterWeb.routes(), RouterWeb.allowedMethods()])
+
+async function root(ctx: Koa.Context) {
+  ctx.logger.info(`from @koa/router.`)
+  ctx.body = `koa app. + @koa/router`
+}
