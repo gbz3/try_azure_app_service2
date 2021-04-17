6f2b790471748573423691d2791d338c0794acb2
gbz3 <1808483+gbz3@users.noreply.github.com>
2021-04-17 20:31:54 +0900

    koa-views/pug 導入

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,23 @@
 import Koa from 'koa'
 
+// __dirname による環境差異を吸収
+const getViewRoot = (dirname: string) => {
+  if (dirname.startsWith('/home/site/wwwroot')) return '/home/site/wwwroot/src/views'  // On Azure WebApps
+  else return __dirname + '/views'  // On WSL2
+}
+
 // Koa2 サーバ初期設定
 const app = new Koa()
 
 // ミドルウェア設定
 import mwServ from 'koa-static'
 import { mwRequestId } from './middlewares/request-id'
+import views from 'koa-views'
 import { mwRouterWeb } from './middlewares/router-web'
 
 app.use(mwServ(__dirname + '/../static'))
 app.use(mwRequestId)
+app.use(views(getViewRoot(__dirname), { autoRender: true, extension: 'pug' }))
 app.use(mwRouterWeb)
 
 // サーバ起動
